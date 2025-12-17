f5a92255d249956a304bbe28f03a660fcb827906
candelariadev <candelariacabreradev@gmail.com>
2025-12-17 17:28:35 -0600

    rutas basicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/home/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,14 +8,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        title: const Text('Flutter + Material <3'),
-      ),
-      body: const _HomeView(
-      ),
-
-
+      appBar: AppBar(title: const Text('Flutter + Material <3')),
+      body: const _HomeView(),
     );
   }
 }
@@ -24,44 +19,38 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-  
-
-
     return ListView.builder(
       itemCount: appMenuItems.length,
-      
-      itemBuilder: (context, index){
+
+      itemBuilder: (context, index) {
         final menuItem = appMenuItems[index];
 
-
         return _CustomListTile(menuItem: menuItem);
-
       },
-      );
+    );
   }
 }
 
 class _CustomListTile extends StatelessWidget {
-  const _CustomListTile({
-    required this.menuItem,
-  });
+  const _CustomListTile({required this.menuItem});
 
   final MenuItem menuItem;
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
       leading: Icon(menuItem.icon, color: colors.primary),
-      trailing:  Icon(Icons.arrow_forward_ios_rounded),
+      trailing: Icon(Icons.arrow_forward_ios_rounded),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){
+      onTap: () {
+        // Navigator.of(context).push(
+        //   MaterialPageRoute<void>(builder: (context) => const ButtonsScreen()),
+        // );
 
-        //TODO: navegar a otra pantalla
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
