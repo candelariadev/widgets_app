d92e0facf5d15aa1819bc1eed33db0f16aabe1d5
candelariadev <candelariacabreradev@gmail.com>
2025-12-18 14:57:53 -0600

    go router configurado

---
+++ lib/presentation/screens/home/screens.dart
@@ -0,0 +1,6 @@
+
+
+
+export 'package:widgets_app/presentation/screens/home/buttons/buttons_screen.dart';
+export 'package:widgets_app/presentation/screens/home/cards/cards_screen.dart';
+export 'package:widgets_app/presentation/screens/home/home_screen.dart';
