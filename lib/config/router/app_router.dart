43e9b16f1cefce8c6e5b94d126dbce03f022d03f
candelariadev <candelariacabreradev@gmail.com>
2025-12-21 14:16:10 -0600

    screens creadas y barril actualizado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:widgets_app/presentation/screens/home/screens.dart';
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 final AppRouter = GoRouter(
   initialLocation: '/',
--- lib/presentation/screens/home/screens.dart
+++
@@ -1,6 +0,0 @@
-
-
-
-export 'package:widgets_app/presentation/screens/home/buttons/buttons_screen.dart';
-export 'package:widgets_app/presentation/screens/home/cards/cards_screen.dart';
-export 'package:widgets_app/presentation/screens/home/home_screen.dart';
