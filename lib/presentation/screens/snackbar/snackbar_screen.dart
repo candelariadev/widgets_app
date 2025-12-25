3419a9af62d7dcf3339c71ff15ca411ec6d1204f
candelariadev <candelariacabreradev@gmail.com>
2025-12-25 16:28:31 -0600

    respaldo

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
 class SnackbarScreen extends StatelessWidget {
-
-static const name = 'snacbar_screen';
+  static const name = 'snacbar_screen';
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final snackbar = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Snackbars y Dialogos')),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Mostrar snackbar'),
+        icon: Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
